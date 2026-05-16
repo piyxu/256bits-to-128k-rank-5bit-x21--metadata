49f40ac3eb2f14d9d3a097972888da1453e7bbee
π y x u(t) <gece12@gmail.com>
2026-05-16 11:24:05 +0300

    Update encode.py

--- encode.py
+++ encode.py
@@ -2,7 +2,8 @@
 # encode.py - 256/128k test
 # MIT LICENSE
 # MESUT ERTURHAN
-# https://github.com/piyxu/256bits-to-128k-rank-4bit-metadata/
+# https://github.com/piyxu/256bits-to-128k-rank-5bit-x21--metadata/
+# Old address piyxu/256bits-to-128k-rank-4bit-metadata/
 # -------------------------------------------------
 
 from math import comb
@@ -58,25 +59,18 @@ C256 = comb(256, 128)
 MASK_256 = (1 << 256) - 1  # 256-bit masking
 
 def encode256(R: int):
-    """
-    R in [0, 16 * C256)
-    Output:
-        bits256 : 256-bit, contains 128 ones
-        m       : 0..15 (4-bit metadata)
-    """
 
-    if not (0 <= R < 16 * C256):
-        raise ValueError("R must be in [0, 16*C(256,128))")
+    if not (0 <= R < 21 * C256):
+        raise ValueError("R must be in [0, 21*C(256,128))")
 
     m   = R // C256
     idx = R  % C256
     bits256 = unrank_256_128(idx)
     return bits256, m
-
+    
 # -------------------------------------------------
 #  MAIN PROGRAM
 # -------------------------------------------------
-
 
 def main():
     rng = random.Random()
@@ -86,8 +80,7 @@ def main():
          open("encode.txt", "w") as f_enc:
 
         for _ in range(num_tests):
-            # R in the range 0 .. 16*C256
-            R = rng.randrange(16 * C256)
+            R = random.getrandbits(256)
 
             # originals.txt: 256-bit view of R (upper bits are discarded by masking)
             ori_bits = format(R & MASK_256, "0256b")
@@ -96,14 +89,12 @@ def main():
             # encode: R -> (balanced_256, m)
             bits256, m = encode256(R)
 
-            meta_bin = format(m, "04b")
+            meta_bin = format(m, "05b")
             enc_bits = "".join(str(b) for b in bits256)
 
-            # encode.txt: 4 bit meta + 256 bit balanced
             f_enc.write(meta_bin + enc_bits + "\n")
 
     print("Encode completed → originals.txt, encode.txt")
 
 if __name__ == "__main__":
     main()
-
